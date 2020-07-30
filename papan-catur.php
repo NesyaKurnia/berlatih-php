e2a20217fd54e58d2c78dacbda18cd83add9c64d
NesyaKurnia <nesya.kurbiadewi@gmail.com>
2020-07-30 09:16:35 +0800

    update

--- papan-catur.php
+++ papan-catur.php
@@ -2,26 +2,25 @@
 
 function papan_catur($angka) {
 // tulis kode di sini
-    $text = '';
-    $jumlah = "";
-    $text .= '<table border=1>';
-    for($i = 0; $i<4; $i++) {
-        $text .= '<tr>';
-        for($j = 0; $j<4; $j++) {
-            $color = '';
-            if(($i %2 == 0 && $j %2 == 0) || ($i %2 == 1 && $j %2 == 1)) {
-                $color = '#000';
+    for($i = 0; $i < $angka; $i++) {
+        for($j = 0; $j < ($angka*2)-1; $j++) {
+            if($j %2 == 1 && $i %2 == 1) {
+                echo " #&nbsp";
             }
-            else $color = '#fff';
-            $text .= '<td width=30 height=30 bgcolor='.$color.'></td>';
+            else if ($j %2 == 0 && $i %2 == 0) {
+                echo " #&nbsp";
+            }
+            else {
+                echo "&nbsp";
+            }
         }
-        $text .= '</tr>';
+        echo "<br>";
     }
-    $text .= '</table>';
 }
 
 // TEST CASES
-echo papan_catur(4) ;
+//echo "Papan Catur 4" . "<br>";
+echo papan_catur(4) . "<br><br>" ;
 /*
 # # # #
  # # #
@@ -29,7 +28,7 @@ echo papan_catur(4) ;
  # # #
  */
 
-echo papan_catur(8);
+echo papan_catur(8) . "<br><br>";
 /* 
 # # # # # # # #
  # # # # # # # 
@@ -41,7 +40,7 @@ echo papan_catur(8);
  # # # # # # #
 */
 
-echo papan_catur(5) ;
+echo papan_catur(5) . "<br><br>" ;
 /*
 # # # # #
  # # # #
